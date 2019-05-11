04a77f7a94d6e56c5921507553bfd4f998d5f1a8
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-05-11 11:09:37 -0400

    reservationForm added and connected

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+// const path = require('path');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hot Restaurant'))
 
+app.get("/reservationForm", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservationForm.html"));
+  });
+
 app.listen(port, () => console.log(`Hot Restaurant app is listening on port ${port}!`))
+
+

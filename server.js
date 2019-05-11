5f2749fe9f4024ea5ce2de3b14cbb94fe84e254d
Brent Abruzese <brent.abruzese@gmail.com>
2019-05-11 10:55:03 -0400

    server stuff added

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+let currentRes = [
+    {
+        userName: 'Brent Abruzese',
+        phone: '848-213-2522',
+        email: 'brent.abruzese@gmail.com',
+        id: 438534
+    }
+]
+
+let waitingList = [
+    {
+        userName: 'Som Ramnani',
+        phone: '800-555-1212',
+        email: 'som@som.com',
+        id: 483984
+    }
+]
+
+let resCount = 0;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/reservations", function(req, res) {
+
+})
+
+app.get('/', (req, res) => res.send('Hot Restaurant'))
+
+app.listen(port, () => console.log(`Hot Restaurant app is listening on port ${port}!`))
